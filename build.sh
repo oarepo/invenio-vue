97daf6433879049fd622fa515418952d0bace1ed
Mirek Simek <miroslav.simek@gmail.com>
2021-04-12 18:23:38 +0200

    jq?

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ yarn bili
 rm -rf dist/src
 
 # set the version in package.json
-jq --arg version $(jq -r ".version" package.json) '.version=$version' library/package.json >dist/package.json
+./node_modules/.bin/jq --arg version $(jq -r ".version" package.json) '.version=$version' library/package.json >dist/package.json
 
 cp README.md dist

fa9f24232e7386e19c47c0e7883fc8b1bd786ce2
Mirek Simek <miroslav.simek@gmail.com>
2021-04-13 08:06:48 +0200

    Exporting wrappers

--- library/index.ts
+++ library/index.ts
@@ -8,3 +8,10 @@ export * from './http'
 export * from './invenio'
 
 export {collection, record} from './routing'
+
+
+// @ts-ignore // workaround for https://github.com/ezolenko/rollup-plugin-typescript2/issues/129
+export {default as CollectionWrapper} from './components/CollectionWrapper.vue'
+
+// @ts-ignore // workaround for https://github.com/ezolenko/rollup-plugin-typescript2/issues/129
+export {default as RecordWrapper} from './components/RecordWrapper.vue'
